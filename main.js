2a878e5f884746e89c582f40315907f8d007d93d
Zahar Klimenko <75505125+Edara-zahar@users.noreply.github.com>
2022-08-21 15:35:07 +0300

    step #2

---
+++ main.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	$('.container__slider').slick({
+		arrows:true,
+		dots:false,
+		slidesToShow:4,
+		autoplay:true,
+		speed:1000,
+		autoplaySpeed:700,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	})
+
+
+	setInterval(() => {
+    if ($(window).scrollTop() > 0 && $('.header__top').hasClass('header__top--open') === false) {
+      $('.burger').addClass('burger--follow')
+    } else {
+      $('.burger').removeClass('burger--follow')
+    }
+  }, 0)
+	$('.burger').on('click', function (e) {
+		e.preventDefault()
+		$('.header__top').toggleClass('header__top--open')
+	})
+	
+
+$('.logoholder__acc-link').on('click', function (e) {
+	e.preventDefault()
+	$(this).toggleClass('logoholder__acc-link--active')
+	$(this).children('.logoholder__acc-text').slideToggle()
+})
+
+});
